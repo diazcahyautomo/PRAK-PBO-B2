c879920e39402b73b13262b469f8b1e66406cbda
Diaz Cahya Utomo <163900291+diazcahyautomo@users.noreply.github.com>
2025-03-25 16:24:13 +0700

    Update CivitasAkademik.java

--- Responsi PBO B2 DIAZ/CivitasAkademik.java
+++ Responsi PBO B2 DIAZ/CivitasAkademik.java
@@ -1,5 +1,7 @@
-/* nama : enzokulin 
- * nim : 24060123140060
+/*  Nama : Diaz Cahya Utomo
+ *  Nim : 24060123140213
+ *  Tanggal : 25 Maret 2025
+ *  RESPONSI PBO B2
  */
 
 public abstract class CivitasAkademik {
