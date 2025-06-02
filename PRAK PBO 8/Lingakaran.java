159b7e3905f3eae8a8d8971ee6209fa7b8399ff5
Diaz Cahya Utomo <163900291+diazcahyautomo@users.noreply.github.com>
2025-06-02 15:42:56 +0700

    Add files via upload

---
+++ PRAK PBO 8/Lingakaran.java
@@ -0,0 +1,11 @@
+public class Lingakaran extends BangunDatar{
+    private double jejari;
+
+    public Lingakaran(double jejari){
+        this.jejari = jejari;
+    }
+
+    public double hitungKeliling(){
+        return 2*jejari*3.14;
+    }
+}
